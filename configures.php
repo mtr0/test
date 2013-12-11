60e5582a26619a0a5c230d976593a9da352e3271
mtr0 <mt_rand@open.by>
2013-12-11 16:11:04 +0300

    a

---
+++ configures.php
@@ -0,0 +1,1 @@
+﻿<?php		class Configures{		public static $db_host = "localhost";		public static $db_user = "root";		public static $db_pass = "vertrigo";		public static $db_name = "test";			}?>
